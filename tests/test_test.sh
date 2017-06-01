09660d8b87faa033657915fe0fe6805b8bf0280f
Annie Pham <apham024@ucr.edu>
2017-06-01 05:03:23 +0000

    updated and fixed some test cases

--- tests/test_test.sh
+++ tests/test_test.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #tests for the test command
 
-[ -e /home/ubuntu/workspace/cs100/rshell ] && echo exists
-test -f /home/ubuntu/workspace/cs100/rshell/LICENSE && echo file exists
+[ -e bin ] && echo exists
+test -f LICENSE && echo file exists
 
 mkdir testdir
-test -d /home/ubuntu/workspace/cs100/rshell/testdir && echo is a directory
+test -d testdir && echo is a directory
 
 touch poop.h
-test -f /home/ubuntu/workspace/cs100/rshell/poop.h && echo file exists
+test -f poop.h && echo file exists
 rm poop.h
-test -f /home/ubuntu/workspace/cs100/rshell/poop.h || echo file poop.h does not exist
+test -f poop.h || echo file poop.h does not exist
 
 rm -r testdir
-[ -d /home/ubuntu/workspace/cs100/rshell/testdir ] || echo no such directory found
+[ -d testdir ] || echo no such directory found
 
 
 
